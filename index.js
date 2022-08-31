2c0da48062fc73649563ff00c07b23c90c158876
Israel Vasquez <ing.visrael@gmail.com>
2022-08-31 13:13:20 -0400

    upload just index.js
    upload just index.js gleap changes

--- index.js
+++ index.js
@@ -3,10 +3,12 @@
 import { AppRegistry, Platform } from 'react-native';
 import moment from 'moment';
 import App from './App';
-
+import Gleap from 'react-native-gleapsdk';
 import { i18next } from './src/i18n';
 import 'moment-timezone';
 import 'moment/locale/es';
+
+Gleap.initialize('P26aq9PfN8vTJNCG94pBGnLHpiabvUJ5');
 
 const appName = Platform.select({
   ios: () => 'coJobcoreTalent',
@@ -16,3 +18,4 @@ const appName = Platform.select({
 moment.locale(i18next.language);
 
 AppRegistry.registerComponent(appName, () => App);
+ 
